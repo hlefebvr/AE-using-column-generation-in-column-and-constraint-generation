da14b70dd552ad4bb767fe1c7865d13b1443e9b3
Henri Lefebvre <hri.lefebvre@gmail.com>
2025-01-16 14:44:30 +0100

    edit run_all

--- code/JSP/experiments/run_all.sh
+++ code/JSP/experiments/run_all.sh
@@ -16,10 +16,10 @@ COUNTER=0
 
 for FILE in $PROJECT_DIRECTORY/$INSTANCE_DIRECTORY/*
 do
-  for GAMMA in 3 4
+  for GAMMA in 3 #4
   do
 
-    for STD_PHASE_TIME_LIMIT in 10800 # 120 60
+    for STD_PHASE_TIME_LIMIT in 10800 0 # 120 60
     do
 
       for WITH_HEURISTIC in 1 # false true
